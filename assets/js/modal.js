bc65d798d75e74c719d444891a9e544796d174d0
matt3541 <matt@mattunderscore.co>
2017-03-23 16:43:42 -0700

    un cache modal.js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -62,7 +62,7 @@
 			modal.style.display = "none";
 		}
 		
-				// Get the modal-------------------------------------------------------------
+		// Get the modal-------------------------------------------------------------
 		var modal = document.getElementById('myModal4');
 
 		// Get the image and insert it inside the modal - use its "alt" text as a caption
@@ -77,6 +77,27 @@
 
 		// Get the <span> element that closes the modal
 		var span = document.getElementsByClassName("close")[3];
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() { 
+			modal.style.display = "none";
+		}
+
+		// Get the modal-------------------------------------------------------------
+		var modal = document.getElementById('myModal5');
+
+		// Get the image and insert it inside the modal - use its "alt" text as a caption
+		var img = document.getElementById('myImg5');
+		var modalImg = document.getElementById("img05");
+		var captionText = document.getElementById("caption5");
+		img.onclick = function(){
+			modal.style.display = "block";
+			modalImg.src = this.src;
+			captionText.innerHTML = this.alt;
+		}
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[4];
 
 		// When the user clicks on <span> (x), close the modal
 		span.onclick = function() { 
